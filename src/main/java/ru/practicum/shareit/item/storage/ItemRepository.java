ad1be76a30d32c0fe2b248a8b646ef70c9bd54a2
Islandec235 <nrgjay@yandex.ru>
2024-06-16 21:08:02 +0300

    fix: убраны лишние импорты

--- src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     void deleteById(Long id);
--- src/main/java/ru/practicum/shareit/user/storage/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserRepository.java
@@ -3,9 +3,6 @@ package ru.practicum.shareit.user.storage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
